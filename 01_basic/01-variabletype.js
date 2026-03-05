ec2edadb0f524d390872e305f2b62d7f7e39ebbd
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-05 09:41:06 +0000

    01-variabletype.js

---
+++ 01_basic/01-variabletype.js
@@ -0,0 +1,9 @@
+const acountId=12345
+let acountEmail="ankitashahi@google.com"
+var acountPassword="34678"
+acountPassword="2134567890"
+ //acountId=2//
+// acountEmail="hiteshchoudhary@gmail.com"
+
+ console.log(acountPassword);
+ 
