67b62f0448997e13cae58dc6ec9abb2daec64774
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-05 09:35:52 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,17 @@
+let name ="ankita"
+let num=123;
+let isLoggin=false;
+let state;
+
+
+console.log(typeof "ank");
+
+
+// number//2 the power 53
+//bigInt
+//string '',""
+//boolen
+//null emprty// type of null object
+//Symbol// uniqnes
+//undefine
+
