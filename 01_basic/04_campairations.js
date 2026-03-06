c947cd03d05cec111f2da53b4eca208eeb8bfd51
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-06 08:38:42 +0000

    04_campairations.js

---
+++ 01_basic/04_campairations.js
@@ -0,0 +1,12 @@
+//console.log(4<9);
+//console.log(null<0);
+console.log(null>=0);
+
+//console.log(null==0);
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined <0);
+//=== stricly chek data types
+console.log("2"===2);
+
+
