fac3bf09cce7d83e02356963f9f066c8dd2b17e3
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-06 08:08:23 +0000

    03_dataytypeconversion.js

---
+++ 01_basic/03_datatypeconversion.js
@@ -0,0 +1,59 @@
+
+let totalNum="123abc"
+let value=Number(totalNum)
+//console.log(typeof value);
+//console.log(value);
+
+let score=null;
+let value1=Number(score);
+//console.log(score);
+//console.log(typeof score);
+
+//console.log( value1);
+let num1="sit"
+let bolen= Boolean(num1)
+//console.log(bolen);
+
+/***********************operation*****************************/
+let value2=34
+let negValue=-value2
+//console.log(negValue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2%2);
+
+str1=" ankita "
+str2="shahi"
+str3=(str1+str2);
+//console.log(str3)
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log(1+2);
+//console.log(1+2*4%2); wrong
+//console.log(((1+2)*10)%2)
+//console.log(+true);
+//console.log(+"");
+ let gameCounter=200;
+ gameCounter++;
+ console.log(gameCounter);
+ ++gameCounter
+ console.log(gameCounter);
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
