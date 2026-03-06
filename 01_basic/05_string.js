8750bf54c473a9f07508f13e7ebc973d4676bdb7
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-06 13:22:37 +0000

    05_string.js

---
+++ 01_basic/05_string.js
@@ -0,0 +1,7 @@
+let name="aakash singh"
+let repoCount=34
+//console.log(name + repoCount +"value");
+console.log(`my Name is ${name} and my repocount is ${repoCount} and my mailid `);
+const newName= new String('ankitashahi')
+console.log(newName[1]);
+console.log(newName[0]);
