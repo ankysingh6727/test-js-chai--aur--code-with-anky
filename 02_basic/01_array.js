040a9f660960447ca6ad8e46accdc5087c63c3b6
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-09 09:36:55 +0000

    array

---
+++ 02_basic/01_array.js
@@ -0,0 +1,36 @@
+let arr=[0,1,2,3,4]
+//console.log(arr)
+//console.log(typeof arr)
+//console.log(arr [2]);
+let heros=["srk","amir"];
+let myarr= new Array(1,4,6,8,7,8)
+//console.log(myarr);
+
+
+//Array Method
+
+myarr.push(45);
+//console.log(myarr);
+myarr.pop();
+//console.log(myarr);
+myarr.unshift(30)
+//console.log(myarr);
+myarr.shift();
+//console.log(myarr);
+//console.log(myarr.includes(9));
+//console.log(myarr.includes(8));
+//console.log(myarr.indexOf(8));
+ let myNewArr=myarr.join();
+ //console.log(myarr)
+ //console.log(  myNewArr)
+myarr.slice(2,3)
+//console.log(myarr);
+
+
+//slice and splice
+
+
+
+
+
+
