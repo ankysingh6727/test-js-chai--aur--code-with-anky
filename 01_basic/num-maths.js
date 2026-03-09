632c617c7f937126cfcc7315bc97ac3bfd2368c5
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-09 07:05:28 +0000

    num-math.js

---
+++ 01_basic/num-maths.js
@@ -0,0 +1,31 @@
+const score=500
+//console.log(score);
+const balance= new Number(200)
+//console.log(balance);
+//console.log(balance.toString());
+//console.log(balance.toFixed(2));
+const newNum=23.4567;
+//console.log(newNum.toPrecision(3));
+//console.log(newNum.toPrecision(4));
+const otherNum=1000000
+//console.log(otherNum.toLocaleString());
+//console.log(otherNum.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++++ Maths++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(((Math.random())*10)+1);
+console.log(Math.floor(((Math.random())*10)+1));
+min=10
+max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
