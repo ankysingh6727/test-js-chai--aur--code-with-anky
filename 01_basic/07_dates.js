45cd754e17f0e12dd1f8ec10b2910a2adfd82221
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-09 08:09:39 +0000

    07-dates.js

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,14 @@
+let myDate= new Date();
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleDateString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toJSON);
+//console.log(typeof myDate);
+let otherDate= new Date(2026,0,25)
+console.log(otherDate);
+console.log(otherDate.toString());
+
+
+
+
+
