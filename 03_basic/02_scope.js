cff07dd1e711ebb7391e820f944f8fff0db5ccf5
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-10 09:54:02 +0000

    scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,26 @@
+let a=1
+var b=2
+const c=3
+// console.log(a)
+// console.log(b)
+// console.log(c)
+{var b=300
+    let a=20
+    //console.log(a)
+    
+}
+//console.log(b)
+function one(sark){
+   const username="ankita shahi"
+   
+   function two(){
+    const website="ankit2122345"
+console.log(username)
+console.log(website)
+   }
+//console.log(website)
+//two()
+
+}
+one()
+
