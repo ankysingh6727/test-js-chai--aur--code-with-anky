36cc99b7e06440d038a97cd35514fb430a169ce5
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-10 06:57:22 +0000

    function

---
+++ 03_basic/01_function.js
@@ -0,0 +1,31 @@
+ function sayMy(){
+    console.log("Rima");
+    console.log("priya");
+    console.log("pooja");
+    console.log("Gudiya");
+    console.log("Garima");
+    
+ }
+ //sayMy()
+//  function addtwonum(num1,num2){
+//     console.log(num1+num2)
+//  }
+//  addtwonum(4,7)
+//  addtwonum(20,50)
+// function addTwoNum(num1,num2){
+//     //let result=num1+num2;
+//     //return result
+// return num1+num2
+// }
+// const result=addTwoNum(9,12)
+// //console.log( "result=", result)
+// console.log( `result= ${result}`)
+function userLoggedIN(username="rima"){
+    if(!username){
+        console.log("plecse Enter the Username");
+      return  
+    }
+return(`${username} just loggedIn `)
+}
+//console.log(userLoggedIN("ankita shahi"))
+console.log(userLoggedIN("anky"))
