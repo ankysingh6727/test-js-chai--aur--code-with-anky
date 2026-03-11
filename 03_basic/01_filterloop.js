8edc0376c01a1600d1fc619c38c106c49e60ca55
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-11 13:06:20 +0000

    filterloop

---
+++ 03_basic/01_filterloop.js
@@ -0,0 +1,45 @@
+//  const coding=["java","c++","phython","reactjs"]
+//  const value1=coding.forEach( (item)=>{
+//     console.log(item)
+//  })
+//  console.log(value1)
+
+
+ const myNumber=[1,2,3,4,5,6,7,8,9,10]
+// const value =myNumber.filter((num)=>    (num> 4))
+ const mynewnum=[]
+const value=myNumber.forEach((num)=>{
+    if (num>4) {
+        mynewnum.push(num)
+        
+    }
+})
+
+console.log(mynewnum)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
