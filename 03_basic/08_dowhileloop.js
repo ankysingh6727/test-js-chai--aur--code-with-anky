59c32072a32cc2978102b45fe735c3b4f6b04497
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-11 09:31:29 +0000

    dowhileloop

---
+++ 03_basic/08_dowhileloop.js
@@ -0,0 +1,25 @@
+
+// let index=0
+// while (index<= 10) {
+//     //console.log(`Value of index ${index}`);
+//     index=index+2
+    
+    
+// }
+let MyArray=["sarkhu","ajay","salmaan","shahid",]
+let Arr=0;
+ while (Arr < MyArray.length) {
+    console.log(`heros name is ${MyArray[Arr]}`)
+    Arr = Arr+1
+    
+ }
+ let score=20
+ do {
+    console.log(` score is ${score}`)
+    score++
+    
+ } while (score<10);
+
+ 
+ // Arrayloop
+
