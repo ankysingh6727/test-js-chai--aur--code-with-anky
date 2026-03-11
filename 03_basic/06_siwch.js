069e6f3a6ec4709887fff992ce9e58e9ae73a449
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-11 06:57:26 +0000

    switch.js

---
+++ 03_basic/06_siwch.js
@@ -0,0 +1,37 @@
+// const
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month=3
+//const month="march"
+switch (month) {
+    case 1:
+        //case "jan":
+    console.log('january')
+        
+        break;
+    case 2:
+    console.log('feb')
+        
+        break;
+    case 3:
+    console.log('march')
+        break;
+    case 4:
+    console.log('april')
+        
+        break;
+    case 3:
+    console.log('may')
+        
+        break;
+
+    default:
+        console.log("defult value")
+        break;
+}
