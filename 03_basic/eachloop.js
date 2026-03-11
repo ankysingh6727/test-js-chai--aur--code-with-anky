9951216a737e1a5bed71286f6b542208f8055158
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-11 10:56:50 +0000

    forEach loop

---
+++ 03_basic/eachloop.js
@@ -0,0 +1,21 @@
+const coding=["c++", "java", "javascript","reactjs"]
+
+// coding.forEach(  function (val1){
+//     console.log(val1);
+// })
+coding.forEach( (val1)=>{
+   // console.log(val1)
+})
+const proggerming=[
+    {langugename:"java script",
+        price:"999"
+    },
+    {
+        langugename:"c++",
+        price:"799"
+    }
+
+]
+proggerming.forEach( (item)=>{
+    console.log(item.price)
+})
