705f93d58b1685d1c606f0cfb7dfa62555e55456
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-11 10:31:50 +0000

    for of and for in loop

---
+++ 03_basic/one.js
@@ -0,0 +1,19 @@
+const  Myobjec={
+    js:"javascript",
+    cpp:"c++",
+    rub:"rubby",
+    swifth:"swift of apple"
+
+}
+for (const key in Myobjec) {
+   // console.log(`${key} for shoutcut ${Myobjec[key]}`)
+    
+    
+    
+}
+const Arr=["javaskript","c++","java","c"]
+for (const key in Arr) {
+ console.log(Arr[key]);
+    
+    
+}
