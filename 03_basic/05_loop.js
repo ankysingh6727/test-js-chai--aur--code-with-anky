875f4270a1fc10ff3d721dbba05d51453200f00f
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-11 06:12:00 +0000

    loop.js

---
+++ 03_basic/05_loop.js
@@ -0,0 +1,30 @@
+//if
+const temp=40
+//  if (temp > 50) {
+//     console.log("ankita is loggedin")
+    
+//  }else{
+//     console.log("temp is greater then 50")
+//  }
+ const score=300
+//  if (score>400) {
+//     const power="fly"
+//     console.log(` score is less then 300 ${power}`)
+//  }
+//   console.log(` score is less then 300 ${power}`)
+const blance=500
+if (blance>900) {
+    console.log("yes blance is less then 1000 and blance is 900")
+}else if(blance>750){
+    console.log("yes blance is less then 1000 and blance is 750")
+}else if(blance==1000){
+    console.log("yes blance is less then 1000 and blance is 900")
+    
+}else{
+    console.log("blance")
+}
+ 
+ 
+ 
+
+ 
