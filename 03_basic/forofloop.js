0ff4c66df942aa766ebd0121bdbcabbb8abafc09
Ankita Shahi <ankysingh6727@gmail.com>
2026-03-11 10:11:06 +0000

    forofloop

---
+++ 03_basic/forofloop.js
@@ -0,0 +1,61 @@
+//for of loop
+
+// ["","","",]
+// [{},{},{},]
+ const Arr=[1,2,3,4,]
+ for (const num of Arr) {
+  // console.log(num)
+ }
+  let Greeting= "ankita"
+  for (const Greet of Greeting) {
+  // console.log(`Each char of string ${Greet}`)
+   
+  }
+
+//map
+const map=new Map()
+map.set("IN","America","Westendis")
+map.set("US","Canada", "dubai")
+map.set("UP","Bihar","Rajasthan")
+
+//console.log(map);
+for (const [key,Value] of map) {
+   console.log(key ,":-", Value)
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
